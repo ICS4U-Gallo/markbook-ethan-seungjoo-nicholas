892ba00e5cf1da1859f588489bde37a8fd70f41b
ethan_alvares <47458407+ethanalvares@users.noreply.github.com>
2020-09-23 13:22:39 -0400

    Update markbook.py

--- markbook.py
+++ markbook.py
@@ -1,6 +1,6 @@
 """
 Markbook Application
-Group members: 
+Group members: Ethan, Seung Joo, Nicholas
 """
 from typing import Dict
 
@@ -15,17 +15,42 @@ def create_assignment(name: str, due: str, points: int) -> Dict:
     Returns:
         Assignment as a dictionary.
     """
-    return {}
+    assignment_dict = {
+        "name": name, 
+        "due": due, 
+        "points": points
+    }
+    
+    return assignment_dict
 
 
 def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
     """Creates a classroom dictionary"""
-    return {}
+    classroom = {
+        'course_code': course_code,
+        'course_name': course_name,
+        'period': period,
+        'teacher': teacher,
+        'student_list': [],
+        'assignment_list': []
+    }
+    return classroom
 
+classroom = create_classroom('ics4u', 'computer science', 2, 'Mr. Gallo')
+print(classroom)
 
 def calculate_average_mark(student: Dict) -> float:
     """Calculates the average mark of a student"""
-    return 0
+    student['marks'].sort()
+    
+    total = 0
+    for mark in student['marks']:
+        total += mark
+    average = total / len(student['marks'])
+    return average
+# answer = calculate_average_mark()
+
+print(calculate_average_mark(student = {"marks": [50, 100]}))
 
 
 def add_student_to_classroom(student: Dict, classroom: Dict):
@@ -35,7 +60,11 @@ def add_student_to_classroom(student: Dict, classroom: Dict):
         student: Student dict
         classroom: The classroom to add the student to
     """
-    pass
+    classroom['student_list'].append(student.items())
+    #classroom['student_list'].update(student)
+
+    return classroom
+
 
 
 def remove_student_from_classroom(student: Dict, classroom: Dict):
@@ -45,7 +74,8 @@ def remove_student_from_classroom(student: Dict, classroom: Dict):
         student: The student to be removed
         classroom: the class from which the student will be removed.
     """
-    pass
+    classroom['student_list'].remove(student.items())
+    return classroom
 
 
 def edit_student(student: Dict, **kwargs: Dict):
@@ -57,4 +87,6 @@ def edit_student(student: Dict, **kwargs: Dict):
             data that needs to be changed. Can come in the form
             of a dictionary.
     """
-    pass
+    student["first_name"] = kwargs["first_name"]
+    student["last_name"] = kwargs["last_name"]
+    return student["first_name"]
