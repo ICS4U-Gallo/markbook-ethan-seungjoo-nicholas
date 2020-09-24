ef655b62f542123143eb68c1dea3a4105e397e63
N.Smith19 <43012163+NSmith019@users.noreply.github.com>
2020-09-24 13:59:02 -0400

    start UI

--- markbook.py
+++ markbook.py
@@ -2,6 +2,8 @@
 Markbook Application
 Group members: Ethan, Seung Joo, Nicholas
 """
+import json
+import os
 from typing import Dict
 
 
@@ -23,7 +25,6 @@ def create_assignment(name: str, due: str, points: int) -> Dict:
     
     return assignment_dict
 
-
 def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
     """Creates a classroom dictionary"""
     classroom = {
@@ -36,9 +37,6 @@ def create_classroom(course_code: str, course_name: str, period: int, teacher: s
     }
     return classroom
 
-classroom = create_classroom('ics4u', 'computer science', 2, 'Mr. Gallo')
-print(classroom)
-
 def calculate_average_mark(student: Dict) -> float:
     """Calculates the average mark of a student"""
     student['marks'].sort()
@@ -50,8 +48,6 @@ def calculate_average_mark(student: Dict) -> float:
     return average
 # answer = calculate_average_mark()
 
-print(calculate_average_mark(student = {"marks": [50, 100]}))
-
 
 def add_student_to_classroom(student: Dict, classroom: Dict):
     """Adds student to a classroom
@@ -61,7 +57,6 @@ def add_student_to_classroom(student: Dict, classroom: Dict):
         classroom: The classroom to add the student to
     """
     classroom['student_list'].append(student.items())
-    #classroom['student_list'].update(student)
 
     return classroom
 
@@ -90,3 +85,79 @@ def edit_student(student: Dict, **kwargs: Dict):
     student["first_name"] = kwargs["first_name"]
     student["last_name"] = kwargs["last_name"]
     return student["first_name"]
+
+def create_assignment_interface():
+    os.system('clear')
+    print('--------------------------------------------------------------- Create Assignment ---------------------------------------------------------------')
+    name = input('Enter assignment name: ')
+    due_date = input('Enter due date: ')
+    marks = int(input('Enter maximum points: '))
+    print(create_assignment(name, due_date, marks))
+
+def create_classroom_interface():
+    print('--------------------------------------------------------------- Create Classroom ---------------------------------------------------------------')
+    code = input('Enter the course code: ')
+    course_name = input('Enter the course name: ')
+    period = input('Enter the period: ')
+    teacher = input('Enter the teacher name: ')
+    names = input('Enter the student list: ')
+    student_list = names.split()
+    assignments = input('Enter the assignment list: ')
+    assignment_list = assignments.split()
+    
+    # args: course_code: str, course_name: str, period: int, teacher: str
+    # pass
+
+def average_mark_interface():
+    # args: student: Dict -> within dict is ['marks': example marks: 10, 20, 30]
+    pass
+
+def add_student_interface():
+    #args: student: Dict, classroom: Dict
+    pass
+
+def remove_student_interface():
+    #args: student: Dict, classroom: Dict
+    pass
+
+def edit_student_interface():
+    #args: student: Dict, **kwargs: Dict
+    pass
+
+
+def main():
+    working = True
+    print("------------------------------------------------------- Welcome to the Python Teacher Markbook -------------------------------------------------------")
+    while working:
+        user_input = int(input('''
+Choose a program to run:
+Create an Assignment: 1
+Create a Classroom: 2
+Calculate Average Mark: 3
+Add a student to a classroom: 4
+Remove a student from a classroom: 5
+Edit student information: 6
+
+
+Enter # Choice: '''))
+
+        if user_input == 1:
+            create_assignment_interface()
+        elif user_input == 2:
+            create_classroom_interface()
+        elif user_input == 3:
+            average_mark_interface()
+        elif user_input == 4:
+            add_student_interface()
+        elif user_input == 5:
+            remove_student_interface()
+        elif user_input == 5:
+            edit_student_interface()
+        
+        keep_going = input('do you want to do something else y/n')
+        if keep_going == 'n':
+            working = False
+
+
+
+main()
